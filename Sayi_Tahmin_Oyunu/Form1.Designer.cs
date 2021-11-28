bf66f2d91e6cf3d4846eaa52ccb7ca23342c7780
90505 <90505@DESKTOP-O0ONNJH>
2021-11-29 00:41:02 +0300

    Proje dosyası ekle.

---
+++ Sayi_Tahmin_Oyunu/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace Sayi_Tahmin_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Basla = new System.Windows.Forms.Button();
+            this.txtTahmin = new System.Windows.Forms.TextBox();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.btnTahmin = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Basla
+            // 
+            this.Basla.Location = new System.Drawing.Point(54, 34);
+            this.Basla.Name = "Basla";
+            this.Basla.Size = new System.Drawing.Size(75, 23);
+            this.Basla.TabIndex = 0;
+            this.Basla.Text = "Başla";
+            this.Basla.UseVisualStyleBackColor = true;
+            this.Basla.Click += new System.EventHandler(this.Basla_Click);
+            // 
+            // txtTahmin
+            // 
+            this.txtTahmin.Location = new System.Drawing.Point(67, 124);
+            this.txtTahmin.Name = "txtTahmin";
+            this.txtTahmin.Size = new System.Drawing.Size(100, 20);
+            this.txtTahmin.TabIndex = 1;
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Location = new System.Drawing.Point(76, 88);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(35, 13);
+            this.lblSkor.TabIndex = 2;
+            this.lblSkor.Text = "label1";
+            // 
+            // btnTahmin
+            // 
+            this.btnTahmin.Location = new System.Drawing.Point(67, 161);
+            this.btnTahmin.Name = "btnTahmin";
+            this.btnTahmin.Size = new System.Drawing.Size(75, 23);
+            this.btnTahmin.TabIndex = 4;
+            this.btnTahmin.Text = "Tahmin Et";
+            this.btnTahmin.UseVisualStyleBackColor = true;
+            this.btnTahmin.Click += new System.EventHandler(this.btnTahmin_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(73, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(982, 100);
+            this.panel1.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Blue;
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.lblSkor);
+            this.groupBox1.Controls.Add(this.Basla);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.btnTahmin);
+            this.groupBox1.Controls.Add(this.txtTahmin);
+            this.groupBox1.Location = new System.Drawing.Point(508, 185);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 217);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oyun Alanı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Script MT Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(283, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SAYI BULMA OYUNUNA HOŞGELDİNİZ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(196, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(205, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Basla;
+        private System.Windows.Forms.TextBox txtTahmin;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Button btnTahmin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Sayi_Tahmin_Oyunu/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sayi_Tahmin_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void kontrol()
+        {
+            pictureBox1.Enabled = false;
+            pictureBox2.Enabled = false;
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+        }
+
+        int skor;
+        int sayi;
+        
+        private void Basla_Click(object sender, EventArgs e)
+        {
+            skor = 100;
+            lblSkor.Text = "Skor :" + skor.ToString();
+            btnTahmin.Enabled = true;
+            Random rastgele = new Random();
+            sayi = rastgele.Next(101);
+            MessageBox.Show("Aklımdan tuttugum sayiyi bul.");
+            
+            kontrol();
+
+
+        }
+
+        private void btnTahmin_Click(object sender, EventArgs e)
+        {
+            if (skor != 0)
+            {
+                Basla.Enabled = false;
+                Basla.Visible = false;
+                int tahmin = Convert.ToInt16(txtTahmin.Text);
+                if (tahmin < sayi)
+                {
+                    MessageBox.Show("Yukarı");
+                    skor -= 10;
+                }
+                else if (tahmin > sayi)
+                {
+                    MessageBox.Show("Asagi");
+                    skor -= 10;
+                }
+                else
+                {
+                    MessageBox.Show("Tebrikler!");
+                    Basla.Visible = true;
+                    Basla.Enabled = true;
+                    pictureBox2.Enabled = true;
+                    pictureBox2.Visible = true;
+                    panel1.BackColor = Color.Green;
+                }
+            }
+            if(skor==0)
+            {
+                MessageBox.Show("Hakkiniz kalmadi");
+                btnTahmin.Enabled = false;
+                Basla.Enabled = true;
+                Basla.Visible = true;
+                pictureBox1.Enabled = true;
+                pictureBox1.Visible = true;
+                panel1.BackColor = Color.Red;
+            }
+            lblSkor.Text = "Skor :" + skor.ToString();
+            txtTahmin.Clear();
+
+
+        }
+    }
+}
+
